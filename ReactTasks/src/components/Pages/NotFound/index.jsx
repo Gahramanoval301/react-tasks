5f9960c65ffaa022d5234a5a1bcff385cbf6ca97
Gahramanoval301 <lemangahramanoval>
2023-10-21 22:53:34 +0300

    NotFound Page

--- ReactTasks/src/components/Pages/NotFound/index.jsx
+++ ReactTasks/src/components/Pages/NotFound/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <>
+    <h1 style={{color:'darkred', fontSize:50, letterSpacing:6}}>You are hacked by me 😈!!!!!!!!!!!!!!!</h1>
+    <img src='../../../../public/haxcked.gif' alt="" />
+
+    </>
   )
 }
 
