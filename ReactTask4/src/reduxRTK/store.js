77313d4d47ead33846f6badfaa98ede2a9c68c8d
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-02-16 14:55:04 +0300

    changing directories of slices

--- ReactTask4/src/reduxRTK/store.js
+++ ReactTask4/src/reduxRTK/store.js
@@ -1,6 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit'
-import usersReducer from './slices/UsersSlice'
-import PostsReducer from './slices/Postsslice'
+import usersReducer from '../components/slices/UsersSlice'
+import PostsReducer from '../components/slices/Postsslice'
 export const store = configureStore({
     reducer: {
         users: usersReducer,
